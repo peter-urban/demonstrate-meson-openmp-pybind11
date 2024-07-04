ffc41935c6efc372ce51baba317ac07bbde69376
Peter Urban <peter.urban@ugent.be>
2024-07-04 11:52:21 +0200

    Revert "try with openmp pragmas only"
    This reverts commit 2a4fcdf9efba1839abe2c33f7b35af2dc2df7efc.

--- src/demonstrate_meson_openmp/openmp_helper.hpp
+++ src/demonstrate_meson_openmp/openmp_helper.hpp
@@ -2,14 +2,7 @@
 
 #if defined(_OPENMP)
     // only include OpenMP header if it is defined
-    //#include <omp.h>
-    // If OpenMP is not defined, provide fallback functions that simulate
-    // single-threaded behavior
-    #include <thread>
-    inline int omp_get_num_threads() { return std::thread::hardware_concurrency(); }
-    inline int omp_get_max_threads() { return std::thread::hardware_concurrency(); }
-    inline int omp_get_thread_num() { return 0; }
-    inline int omp_get_num_procs() { return std::thread::hardware_concurrency(); }
+    #include <omp.h>
 #else
     // If OpenMP is not defined, provide fallback functions that simulate
     // single-threaded behavior

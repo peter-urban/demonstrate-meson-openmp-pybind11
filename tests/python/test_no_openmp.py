98c08daf1ba5e212be06a8abec0f13ae19e9c9f6
Peter Urban <peter.urban@ugent.be>
2024-07-04 10:41:22 +0200

    import module in tests

--- tests/python/test_no_openmp.py
+++ tests/python/test_no_openmp.py
@@ -2,15 +2,16 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-import demonstrate_meson_no_openmp as demo
-
 # define class for grouping (test sections)
 class Test_no_openmp:
     def test_openmp_is_not_defined_for_compiler_in_header(self):
+        import demonstrate_meson_no_openmp as demo
         assert demo.openmp_is_not_defined_for_compiler_in_header()
 
     def test_openmp_parallel_pragma_is_not_working_in_header(self):
+        import demonstrate_meson_no_openmp as demo
         assert demo.openmp_parallel_pragma_is_not_working_in_header()
 
     def test_openmp_thread_count_is_not_same_as_std_thread_count_in_header(self):
+        import demonstrate_meson_no_openmp as demo
         assert demo.openmp_thread_count_is_not_same_as_std_thread_count_in_header()
--- tests/python/test_openmp_using_library.py
+++ tests/python/test_openmp_using_library.py
@@ -2,18 +2,20 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-import demonstrate_meson_openmp_using_library as demo
 
 # define class for grouping (test sections)
 class Test_openmp_using_library:
     def test_openmp_is_defined_for_compiler_in_header(self):
+        import demonstrate_meson_openmp_using_library as demo
         assert demo.openmp_is_defined_for_compiler_in_header()
         assert demo.openmp_is_defined_for_compiler_in_library()
 
     def test_openmp_parallel_pragma_is_working_in_header(self):
+        import demonstrate_meson_openmp_using_library as demo
         assert demo.openmp_parallel_pragma_is_working_in_header()
         assert demo.openmp_parallel_pragma_is_working_in_library()
 
     def test_openmp_thread_count_is_same_as_std_thread_count_in_header(self):
+        import demonstrate_meson_openmp_using_library as demo
         assert demo.openmp_thread_count_is_same_as_std_thread_count_in_header()
         assert demo.openmp_thread_count_is_same_as_std_thread_count_in_library()

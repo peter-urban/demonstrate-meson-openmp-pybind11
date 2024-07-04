b97d9ab2a9807ab931645839a16204704886b2bc
Peter Urban <peter.urban@ugent.be>
2024-07-04 11:12:35 +0200

    better header/library test case seperation

--- tests/python/test_openmp_using_library.py
+++ tests/python/test_openmp_using_library.py
@@ -8,14 +8,23 @@ class Test_openmp_using_library:
     def test_openmp_is_defined_for_compiler_in_header(self):
         import demonstrate_meson_openmp_using_library as demo
         assert demo.openmp_is_defined_for_compiler_in_header()
+
+    def test_openmp_is_defined_for_compiler_in_library(self):
+        import demonstrate_meson_openmp_using_library as demo
         assert demo.openmp_is_defined_for_compiler_in_library()
 
     def test_openmp_parallel_pragma_is_working_in_header(self):
         import demonstrate_meson_openmp_using_library as demo
         assert demo.openmp_parallel_pragma_is_working_in_header()
+
+    def test_openmp_parallel_pragma_is_working_in_library(self):
+        import demonstrate_meson_openmp_using_library as demo
         assert demo.openmp_parallel_pragma_is_working_in_library()
 
     def test_openmp_thread_count_is_same_as_std_thread_count_in_header(self):
         import demonstrate_meson_openmp_using_library as demo
         assert demo.openmp_thread_count_is_same_as_std_thread_count_in_header()
+
+    def test_openmp_thread_count_is_same_as_std_thread_count_in_library(self):
+        import demonstrate_meson_openmp_using_library as demo
         assert demo.openmp_thread_count_is_same_as_std_thread_count_in_library()
